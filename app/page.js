a210cdec0474d701f7aabee749f33358e292fc75
Moutaz Ali <moutaz.ali.dev@gmail.com>
2024-02-05 13:10:17 +0200

    installing daisyUi along with tailwind

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <p className="text-center p-4 bg-red-500 text-3xl font-bold">Hello world</p>
+    <button className="btn btn-error block text-center m-auto">Click Me</button>
   );
 }
